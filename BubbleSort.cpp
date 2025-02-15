83395263e01ae5377cf8bf91e8ba8fc4253bf333
fhawas87 <jakubwozniak775@gmail.com>
2025-02-15 14:11:29 +0100

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,29 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+void BubbleSort(std::vector<int32_t> &CopyOfInputNumbers) {
+
+    for (int i = 0; i < CopyOfInputNumbers.size(); i++) {
+
+        for (int j = 0; j < CopyOfInputNumbers.size() - i - 1; j++) {
+
+            if (CopyOfInputNumbers.at(j) > CopyOfInputNumbers.at(j + 1)) {
+
+                CopyOfInputNumbers.at(j) ^= CopyOfInputNumbers.at(j + 1);
+                CopyOfInputNumbers.at(j + 1) ^= CopyOfInputNumbers.at(j);
+                CopyOfInputNumbers.at(j) ^= CopyOfInputNumbers.at(j + 1);
+
+            }
+        }
+    }
+
+    std::cout << "\n" << "\n" << "BubbleSort : ";
+
+    for (const auto &Elements : CopyOfInputNumbers) {
+
+        std::cout << Elements << " ";
+    }
+
+    std::cout << "\n" << "\n";
+}
