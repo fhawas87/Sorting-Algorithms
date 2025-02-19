b38764868c085517935861f5615257e826513eef
fhawas87 <jakubwozniak775@gmail.com>
2025-02-20 00:53:07 +0100

    Update SwapAlgorithm.cpp

--- SwapAlgorithm.cpp
+++ SwapAlgorithm.cpp
@@ -1,1 +1,11 @@
+#include <cstdint>
 
+void XORSwapAlgorithm(int32_t &FirstElement, int32_t &SecondElement) {
+
+	if (&FirstElement != &SecondElement) {
+
+		FirstElement ^= SecondElement;
+		SecondElement ^= FirstElement;
+		FirstElement ^= SecondElement;
+	}
+}
