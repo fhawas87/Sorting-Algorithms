f5292b1dfc2a792c33a235b921f43bb8124ce658
fhawas87 <jakubwozniak775@gmail.com>
2025-03-05 13:50:41 +0100

    Update SortingAlgorithmsSort.h

--- SortingAlgorithmsSourceFiles/SortingAlgorithmsSort.h
+++ SortingAlgorithmsSourceFiles/SortingAlgorithmsSort.h
@@ -6,11 +6,11 @@
 
 // Maybe I can define all of functions here ???!!!
 
-void BubbleSort(std::vector<int32_t>        &CopyOfInputNumbers);
-void HeapSort(std::vector<int32_t>          &CopyOfInputNumbers);
-void SelectionSort(std::vector<int32_t>     &CopyOfInputNumbers);
-void InsertionSort(std::vector<int32_t>     &CopyOfInputNumbers);
-void MergeSort(std::vector<int32_t>         &CopyOfInputNumbers);
-void QuickSort(std::vector<int32_t>         &CopyOfInputNumbers);
+void BubbleSort         (std::vector<int32_t> &CopyOfInputNumbers);
+void HeapSort           (std::vector<int32_t> &CopyOfInputNumbers);
+void SelectionSort      (std::vector<int32_t> &CopyOfInputNumbers);
+void InsertionSort      (std::vector<int32_t> &CopyOfInputNumbers);
+void MergeSort          (std::vector<int32_t> &CopyOfInputNumbers);
+void QuickSort          (std::vector<int32_t> &CopyOfInputNumbers);
 
 #endif
